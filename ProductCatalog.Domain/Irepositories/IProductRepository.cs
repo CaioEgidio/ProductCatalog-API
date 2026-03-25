0f996aef23856d5f7f9249e25ad2ca52cc1cb34d
CaioEgidio <78043406+CaioEgidio@users.noreply.github.com>
2026-03-25 14:48:13 -0300

    salvando

---
+++ ProductCatalog.Domain/Irepositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Domain.Irepositories;
+
+public interface IProductRepository
+{
+    //Busca um produto pelo Id
+    Task<Product> GetByIdAsync(Guid id);
+    
+    //Busca a Lista de todos os produtos
+    Task<IEnumerable<Product>> GetAllAsync();
+    
+    //Salva um novo produto no banco
+    Task AddAsync(Product product);
+}
---
+++ ProductCatalog.Domain/Irepositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Domain.Irepositories;
+
+public interface IUserRepository
+{
+    // Busca um usuário pelo ID (Pode retornar nulo se não achar")
+    Task<User?> GetByIdAsync(Guid id);
+    
+    // Busca a lista de todos os usuários
+    Task<IEnumerable<User>> GetAllAsync();
+
+    // Salva um novo usuário no banco
+    Task AddAsync(User user);
+    
+    // Busca um usuário pelo e-mail
+    Task<User?> GetByEmailAsync(string email);
+}
