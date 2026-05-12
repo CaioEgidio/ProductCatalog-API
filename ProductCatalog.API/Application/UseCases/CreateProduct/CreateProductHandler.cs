75a13412d2bf7a78006f65bd94a56ee830cd35e0
CaioEgidio <caio.egidio11@gmail.com>
2026-05-11 23:40:05 -0300

    handler

--- ProductCatalog.API/Application/UseCases/CreateProduct/CreateProductHandler.cs
+++ ProductCatalog.API/Application/UseCases/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,13 @@
 ﻿using ProductCatalog.Domain.Entities;
 using ProductCatalog.API.DTOs;
-
-
+  
 namespace ProductCatalog.API.Application.UseCases.CreateProduct;
 
 public class CreateProductHandler
 {
-    public Product Handle(CreateProductRequest request)
+    public Product Handle(CreateProductRequest request) // Metodo Handler 
     {
-        var product = new Product(
+        var product = new Product( // Cria as entidades 
             request.Nome,
             request.Descricao,
             request.Preco,
@@ -17,3 +16,4 @@ public class CreateProductHandler
         return product;
     }
 }
+
