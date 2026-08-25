bdb31819a625c7a5b936d6dee3b54675373d045e
CaioEgidio <caio.egidio11@gmail.com>
2026-08-25 00:14:02 -0300

    feat: add user creation and retrieval

---
+++ ProductCatalog.Application/UseCases/GetAllUsers/GetAllUsersHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductCatalog.Application.UseCases.GetAllUsers;
+
+public class GetAllUsersHandler
+{
+    
+}
---
+++ ProductCatalog.Application/UseCases/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductCatalog.Application.UseCases.GetUserById;
+
+public class GetUserByIdHandler
+{
+    
+}
